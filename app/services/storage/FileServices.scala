12a7c3f569314f7c024a178599910696923f060f
hashcode <boniface@hashcode.zm>
2015-12-31 22:50:51 +0200

    Refactored Company , Person Class and Updated Routes

--- app/services/storage/FileServices.scala
+++ app/services/storage/FileServices.scala
@@ -7,12 +7,14 @@ import javax.imageio.ImageIO
 
 import com.sksamuel.scrimage.{Format, FormatDetector}
 import conf.util.Util
-import domain.storage.{CompanyFiles, FileMeta, FileResults}
-import org.apache.commons.io.IOUtils
+import domain.storage.{CompanyImages, CompanyFiles, FileMeta, FileResults}
+import org.apache.commons.io.{FileUtils, IOUtils}
 import org.imgscalr.Scalr
-import repository.storage.CompanyFilesRepository
+import repository.storage.{CompanyImagesRepository, CompanyFilesRepository}
 
 import scala.concurrent.Future
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * Created by hashcode on 2015/12/30.
@@ -20,56 +22,74 @@ import scala.concurrent.Future
 object FileServices {
 
 
-  def getFile(company:String, id: String): Future[Option[CompanyFiles]] = {
-   StorageService.findFileById(company,id)
+  def getFile(company: String, id: String): Future[Option[CompanyFiles]] = {
+    StorageService.findFileById(company, id)
   }
 
-  def processFile(data: FileInputStream, meta: FileMeta): Future[Seq[FileResults]] = {
+  def processImage(data: File, meta: FileMeta): Future[Seq[FileResults]] = {
 
     import scala.concurrent.ExecutionContext.Implicits.global
     meta.contentType.startsWith("image") match {
       case true => Future {
+        val ext = getFileExtension(data)
+        val normal = resizeImage(data, ext, 650)
+        val thumb = resizeImage(data, ext, 150)
 
-        val file = new File("/tmp/tmp" + UUID.randomUUID().toString +meta.fileName)
+        val thumBytes = ByteBuffer.wrap(IOUtils.toByteArray(new FileInputStream(thumb)))
 
+        val thumbDataImage = CompanyImages(
+          meta.company,
+          Util.md5Hash(UUID.randomUUID().toString),
+          thumBytes,
+          meta.fileName,
+          meta.contentType
+        )
 
-        val ext = getFileExtension(file)
-        val normal = resizeImage(file, ext, 650)
-        val thumb = resizeImage(file, ext, 150)
+        val normalBytes =  ByteBuffer.wrap(IOUtils.toByteArray(new FileInputStream(normal)))
 
+        val normalDataImage = CompanyImages(
+          meta.company,
+          Util.md5Hash(UUID.randomUUID().toString),
+          normalBytes,
+          meta.fileName,
+          meta.contentType
+        )
 
-        val thumbnailImageId = FilesRepository.save(new FileInputStream(thumb), meta)
+       CompanyImagesRepository.save(thumbDataImage)
         val thumbnailImageMetaData = FileResults(
-          thumbnailImageId.toString,
-          "/api/static/" + thumbnailImageId.toString+"/"+getFileName(thumbnailImageId.toString),
+          thumbDataImage.id,
+          "/api/static/" + thumbDataImage.id+"/"+thumbDataImage.filename,
           Some("Thumbnail"))
 
-        val normalImageId = FilesRepository.save(new FileInputStream(normal), meta)
+        CompanyImagesRepository.save(normalDataImage)
 
-        val normalImageMetaData = FileResults(normalImageId.toString,
-          "/api/static/" + normalImageId.toString+"/"+getFileName(normalImageId.toString),
+        val normalImageMetaData = FileResults(normalDataImage.id,
+          "/api/static/" + normalDataImage.id+"/"+normalDataImage.filename,
           Some("Standard"))
 
         Seq[FileResults](normalImageMetaData, thumbnailImageMetaData)
       }
-      case false => Future {
-        val bytesBufferFile = ByteBuffer.wrap(IOUtils.toByteArray(data))
+      case false =>
+    }
+  }
 
-        val fileData:CompanyFiles = {
-         CompanyFiles(
-           meta.company,
-           Util.md5Hash(UUID.randomUUID().toString),
-           bytesBufferFile,
-           meta.fileName,
-           meta.contentType)
 
-        }
-        CompanyFilesRepository.save(fileData)
-        val fileMetaData = FileResults(fileData.id,
-          "/api/static/" + fileData.id+"/"+fileData.filename,
-          None)
-        Seq[FileResults](fileMetaData)
+  def processFile(data: FileInputStream, meta: FileMeta): Future[Seq[FileResults]] = {
+    Future {
+      val bytesBufferFile = ByteBuffer.wrap(IOUtils.toByteArray(data))
+      val fileData: CompanyFiles = {
+        CompanyFiles(
+          meta.company,
+          Util.md5Hash(UUID.randomUUID().toString),
+          bytesBufferFile,
+          meta.fileName,
+          meta.contentType)
       }
+      CompanyFilesRepository.save(fileData)
+      val fileMetaData = FileResults(fileData.id,
+        "/api/static/" + fileData.id + "/" + fileData.filename,
+        None)
+      Seq[FileResults](fileMetaData)
     }
   }
 
@@ -90,7 +110,7 @@ object FileServices {
   private def resizeImage(file: File, ext: String, size: Int): File = {
     val image = ImageIO.read(file)
     val thumbnail = Scalr.resize(image, size)
-    val resizedImage = new File("/tmp/tmp" + UUID.randomUUID().toString +"."+ ext)
+    val resizedImage = new File("/tmp/tmp" + UUID.randomUUID().toString + "." + ext)
 
     ImageIO.write(thumbnail, ext, resizedImage)
     resizedImage
