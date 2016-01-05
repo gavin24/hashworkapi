8782a6ba374f11d6ca6457a255aaf3faa6aef33c
hashcode <boniface@hashcode.zm>
2016-01-05 13:57:32 +0200

    Added People Images and Attachements

---
+++ app/controllers/company/CompanyDocumentsController.scala
@@ -0,0 +1,33 @@
+package controllers.company
+
+import domain.company.CompanyDocuments
+import play.api.libs.json.Json
+import play.api.mvc.{Action, Controller}
+import services.company.CompanyDocumentsService
+
+/**
+ * Created by hashcode on 2016/01/05.
+ */
+class CompanyDocumentsController extends Controller{
+
+  def createOrUpdate = Action.async(parse.json) {
+    request =>
+      val input = request.body
+      val entity = Json.fromJson[CompanyDocuments](input).get
+      val results = CompanyDocumentsService.save(entity)
+      results.map(result =>
+        Ok(Json.toJson(entity)))
+  }
+
+  def getDocuemtById(company:String, id: String) = Action.async {
+    request =>
+      CompanyDocumentsService.getCompanyDocumentById(company,id) map (result =>
+        Ok(Json.toJson(result)))
+  }
+
+  def getAllCompanyDocuments(company:String) = Action.async {
+    request =>
+      CompanyDocumentsService.getCompanyDocuments(company) map (result =>
+        Ok(Json.toJson(result)))
+  }
+}
