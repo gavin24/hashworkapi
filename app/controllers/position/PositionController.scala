ba31f8f66820dbe8477400eb3472c6dbedf749a6
hashcode <boniface@hashcode.zm>
2016-01-17 13:47:21 +0200

    Added Controllers for Position package

--- app/controllers/position/PositionController.scala
+++ app/controllers/position/PositionController.scala
@@ -5,6 +5,7 @@ import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import services.position.PositionService
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 /**
  * Created by hashcode on 2016/01/11.
@@ -15,6 +16,15 @@ class PositionController extends Controller{
       val entity = Json.fromJson[Position](request.body).get
       PositionService.createOrUpdate(entity) map (result =>
         Ok(Json.toJson(entity)))
+  }
+
+  def batchCreate = Action.async(parse.json) {
+    request =>
+      val entities = Json.fromJson[Set[Position]](request.body).get
+
+      entities.foreach(position=> println("The Code is ",position.code))
+
+        Future{Ok("")}
   }
 
   def getById(company: String, id: String) = Action.async {
